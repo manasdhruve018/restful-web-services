b263cdbf7f72483d4fa15a908aa1d58db59f4a85
Manas Dhruve <manas.dhruve@gmail.com>
2020-08-16 18:54:28 +0530

    Added POST method to a User Resoure & comments too

--- src/main/java/com/manas/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/manas/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,14 +14,22 @@ public class UserResource {
 	@Autowired
 	UserDaoService userService;
 	
+	//Retrieve all Users
 	@GetMapping("/users/all")
 	public List<User> findAllUsers(){
 		return userService.findAll();
 	}
 	
+	//Retrieve a single User
 	@GetMapping("/users/{id}")
 	public User findUser(@PathVariable int id){
 		return userService.findUser(id);
+	}
+	
+	//Create a new User
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user){
+		User savedUser = userService.save(user);
 	}
 	
 }
