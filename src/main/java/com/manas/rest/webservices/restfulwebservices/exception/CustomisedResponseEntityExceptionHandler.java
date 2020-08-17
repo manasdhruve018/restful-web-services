d4e5c8260bb5d71f8021f72c84cc14dee279f003
Manas Dhruve <manas.dhruve@gmail.com>
2020-08-17 18:46:11 +0530

    Add Validation for RESTful Services

--- src/main/java/com/manas/rest/webservices/restfulwebservices/exception/CustomisedResponseEntityExceptionHandler.java
+++ src/main/java/com/manas/rest/webservices/restfulwebservices/exception/CustomisedResponseEntityExceptionHandler.java
@@ -2,8 +2,10 @@ package com.manas.rest.webservices.restfulwebservices.exception;
 
 import java.util.Date;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,4 +29,12 @@ public class CustomisedResponseEntityExceptionHandler extends ResponseEntityExce
 		ExceptionResource exceptionResource = new ExceptionResource(new Date(), ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity(exceptionResource,HttpStatus.NOT_FOUND);
 	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request)
+	{
+		ExceptionResource exceptionResource = new ExceptionResource(new Date(), ex.getMessage(), ex.getBindingResult().toString());
+		return new ResponseEntity(exceptionResource,HttpStatus.BAD_REQUEST);
+	}
+	
 }
