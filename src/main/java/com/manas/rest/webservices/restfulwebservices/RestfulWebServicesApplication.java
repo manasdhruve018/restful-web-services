45f740b5eb5e63612ac3b1287b9d0a825c3ffc4b
Manas Dhruve <manas.dhruve@gmail.com>
2020-08-18 19:43:50 +0530

    Fix from Internationalization

--- src/main/java/com/manas/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/manas/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -4,7 +4,6 @@ import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties.LocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -17,7 +16,7 @@ public class RestfulWebServicesApplication {
 	}
 
 	@Bean
-	public LocaleResolver localeResolver(){
+	public SessionLocaleResolver localeResolver(){
 		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.US);
 		return localeResolver;
