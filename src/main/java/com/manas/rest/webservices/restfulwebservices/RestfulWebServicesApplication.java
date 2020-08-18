ed54fb051ca3f0929b724e063cecb2da94db1649
Manas Dhruve <manas.dhruve@gmail.com>
2020-08-18 19:45:02 +0530

    Revert "Update master code with new functionalities!"

--- src/main/java/com/manas/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ src/main/java/com/manas/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -1,12 +1,7 @@
 package com.manas.rest.webservices.restfulwebservices;
 
-import java.util.Locale;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -15,17 +10,4 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
-	@Bean
-	public SessionLocaleResolver localeResolver(){
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-		localeResolver.setDefaultLocale(Locale.US);
-		return localeResolver;
-	}
-	
-	@Bean
-	public ResourceBundleMessageSource bundleMessageSource(){
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasename("message");
-		return messageSource;
-	}
 }
--- src/main/java/com/manas/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/manas/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,19 +1,11 @@
 package com.manas.rest.webservices.restfulwebservices.helloworld;
 
-import java.util.Locale;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-	
-	@Autowired
-	private MessageSource messageSource;
 	
 	@GetMapping(path="/hello-world")
 	public String helloWorld(){
@@ -30,8 +22,4 @@ public class HelloWorldController {
 		return new HelloWorldBean(String.format("Hello World - %s", name));
 	}
 	
-	@GetMapping(path="/hello-world-internationalization")
-	public String helloWorldInternational(@RequestHeader(name="Accept-Language",required=false) Locale locale){
-		return messageSource.getMessage("good.morning.message",null, locale);
-	}
 }
